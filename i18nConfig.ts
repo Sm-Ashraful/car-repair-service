b1005840818e9598b03880b4ad9194bd187bd984
ashraful <smashraful.dev@gmail.com>
2025-02-09 18:22:39 +0600

    Initial commit from new workspace

---
+++ i18nConfig.ts
@@ -0,0 +1,40 @@
+import { Config } from "next-i18n-router/dist/types";
+
+export const locales: string[] = ["en", "ar"];
+export const localesWithFlags = {
+  en: {
+    name: "English",
+    flagClass: "fi fi-gb", // UK flag for English
+  },
+
+  ar: {
+    name: "Arabic",
+    flagClass: "fi fi-sa", // Saudi Arabia flag for Arabic
+  },
+};
+
+export function refineLocalePrefixForRoute(locale: string) {
+  if (locale === "en") {
+    return "";
+  }
+
+  return "/" + locale;
+}
+
+export function refineRoutePath(path: string, locale: string) {
+  if (path === "/") {
+    if (locale === "en") {
+      return "/";
+    }
+    return "";
+  }
+
+  return path;
+}
+
+const i18nConfig: Config = {
+  locales: locales,
+  defaultLocale: "en",
+};
+
+export default i18nConfig;
