d0c8f7c1e26a532f7cf9d58dafad499aa25f16b7
ashraful <smashraful.dev@gmail.com>
2025-02-14 00:28:01 +0600

    service && package

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
-import { Button } from "./button";
 
 type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
     icon?: React.ReactNode;
